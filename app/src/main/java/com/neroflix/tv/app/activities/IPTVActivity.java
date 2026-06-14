8403efc2350d4e4c52ffd5ae60e117ecccc89650
sasuw30 <arenaofvalorph937@gmail.com>
2026-06-14 22:06:29 +0800

    Add HorizontalScrollView import

--- app/src/main/java/com/neroflix/tv/app/activities/IPTVActivity.java
+++ app/src/main/java/com/neroflix/tv/app/activities/IPTVActivity.java
@@ -12,6 +12,7 @@ import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.HorizontalScrollView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
